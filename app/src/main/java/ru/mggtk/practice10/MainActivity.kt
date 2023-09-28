b591aae3bd714a052bf0dd5f70d3cfe8bd2d6c09
skr0l <lpdimakot11@gmail.com>
2023-09-28 17:06:16 +0300

    first commit

---
+++ app/src/main/java/ru/mggtk/practice10/MainActivity.kt
@@ -0,0 +1,29 @@
+package ru.mggtk.practice10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickVisibleChange(view: View){
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+
+        button1.setOnClickListener{
+            button1.visibility = View.VISIBLE
+            button2.visibility = View.INVISIBLE
+        }
+
+        button2.setOnClickListener{
+            button2.visibility = View.VISIBLE
+            button1.visibility = View.INVISIBLE
+        }
+    }
+
+}
